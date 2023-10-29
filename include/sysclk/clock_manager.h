1aefd52425d1f8a0f84063dfb312211b0fae1efe
MasaGratoR <masagrator@gmail.com>
2023-10-29 13:40:41 +0100

    Add files via upload

---
+++ include/sysclk/clock_manager.h
@@ -0,0 +1,36 @@
+/*
+ * --------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
+ * wrote this file. As long as you retain this notice you can do whatever you
+ * want with this stuff. If you meet any of us some day, and you think this
+ * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
+ * --------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "board.h"
+
+typedef struct
+{
+    uint8_t enabled;
+    uint64_t applicationId;
+    SysClkProfile profile;
+    uint32_t freqs[SysClkModule_EnumMax];
+    uint32_t realFreqs[SysClkModule_EnumMax];
+    uint32_t overrideFreqs[SysClkModule_EnumMax];
+    uint32_t temps[SysClkThermalSensor_EnumMax];
+    int32_t power[SysClkPowerSensor_EnumMax];
+} SysClkContext;
+
+typedef struct
+{
+    union {
+        uint32_t mhz[SysClkProfile_EnumMax * SysClkModule_EnumMax];
+        uint32_t mhzMap[SysClkProfile_EnumMax][SysClkModule_EnumMax];
+    };
+} SysClkTitleProfileList;
+
+#define SYSCLK_FREQ_LIST_MAX 32
